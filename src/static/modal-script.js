38efe2bd9cf589c28bd7ac76516e47f609730c57
SALMAN <elfadili.salman@gmail.com>
2018-12-18 12:42:11 +0100

    change static files directory

---
+++ src/static/modal-script.js
@@ -0,0 +1,2 @@
+var $ = require("jquery");
+$(".modal").each(function(l){$(this).on("show.bs.modal",function(l){console.log("Show"); var o=$(this).attr("data-easein");"shake"===o?$(".modal-dialog").velocity("callout."+o):"pulse"===o?$(".modal-dialog").velocity("callout."+o):"tada"===o?$(".modal-dialog").velocity("callout."+o):"flash"===o?$(".modal-dialog").velocity("callout."+o):"bounce"===o?$(".modal-dialog").velocity("callout."+o):"swing"===o?$(".modal-dialog").velocity("callout."+o):$(".modal-dialog").velocity("transition."+o)})});
