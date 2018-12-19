6dc0f658cc06e8634a3afabbb92dd1cc43e45a2b
SALMAN <elfadili.salman@gmail.com>
2018-12-19 01:35:23 +0100

    finished updateIdea form that's inside ideaModal

--- src/components/crazyIdeas/UpdateIdea.js
+++ src/components/crazyIdeas/UpdateIdea.js
@@ -6,7 +6,8 @@ import '../../static/modal-script';
 class UpdateIdea extends Component {
     state = {
         idea: this.props.idea,
-        formState: false
+        formState: false,
+        ideaChanged: 'disabled'
     }
     componentDidUpdate(prevProps){
         if(this.props.idea !== prevProps.idea) {
@@ -18,6 +19,26 @@ class UpdateIdea extends Component {
     toggle = () => {
         this.props.toggle()
         this.setState({formState:false})
+    }
+    changeHandler = (e) => {
+        let target = e.target
+        if (target.id==='title' && target.value!=null && target.value!=='') {
+            this.setState({ ideaChanged: '' })
+        } else if(target.id==='content') {} 
+        else {this.setState({ ideaChanged: 'disabled' })}
+
+        let idea = {...this.state.idea}
+        idea[target.id] = target.value
+        this.setState({idea})
+    }
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log("Submit: "+this.state.idea.id)
+        /* let ideaChanged = this.state.ideaChanged
+        if (ideaChanged==='') {
+            this.addIdea(this.state.idea)
+            this.initializeState()
+        } */
     }
 
     render() {
@@ -36,7 +57,18 @@ class UpdateIdea extends Component {
                     </div>
                     ) : (
                     <form onSubmit={this.submitHandler}>
-                    <Button onClick={() => this.setState({formState:false})}>Cancel</Button>
+                        <div className="md-form">
+                            <MDBInput id="title" hint="Idea title" className="text-center"
+                                    onChange={this.changeHandler} value={this.state.idea.title} />
+                            <MDBInput id="content" type="textarea" rows="5" hint="So What's your Crazy Idea..." className="mb-0"
+                                    onChange={this.changeHandler} value={this.state.idea.content} />
+                        </div>
+                        <ModalFooter>
+                            <Button color="#FFF" className="btn-link" onClick={() => this.setState({formState:false})}>Cancel</Button>
+                            <Button rounded outline color="save" type="submit" className={this.state.ideaChanged} >
+                                <span className="font-weight-bold">Save</span>
+                            </Button>
+                        </ModalFooter>
                     </form>
                     )
                     }
